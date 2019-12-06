415f006d614b01e3e1b3e4a1a81608d18f279f3e
Gordon <gordon@gordon.nongkynrih>
2019-12-06 11:00:34 +0530

    Add votes answers and views

--- app/Question.php
+++ app/Question.php
@@ -1,28 +1,44 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
-
 class Question extends Model
 {
-    protected $fillable = ['title','body'];
-    public function user(){
-        return $this->belongsTo(User::class);
-    }
+    protected $fillable = ['title', 'body'];
     
-    public function setTitleAttribute($value){
+    public function user() {
+        return $this->belongsTo(User::class);
+    }    
+    public function setTitleAttribute($value)
+    {
         $this->attributes['title'] = $value;
-        $this->attributes['slug'] = Str::slug($value);   //str_slug converts to slug format
+        $this->attributes['slug'] = str_slug($value);
     }
     public function getUrlAttribute()
     {
-        return route("questions.show",$this->id);
+        return route("questions.show", $this->slug);
     }
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
     }
-
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
+    }
+    public function getBodyHtmlAttribute()
+    {
+        return \Parsedown::instance()->text($this->body);
+    }
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+        // $question->answers->count()
+        // foreach ($question->answers as $answer)
+    }
 }
